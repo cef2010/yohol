78f43cbaf51b5a1259c4ca9ad7b4b53308b47d4f
cef2010 <cef2010@gmail.com>
2015-09-30 19:47:20 -0600

    update paperclip sizes

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,6 +1,6 @@
 class Goal < ActiveRecord::Base
   belongs_to :user
-  has_attached_file :picture, styles: { med: "400x400" }
+  has_attached_file :picture, styles: { med: "400x400#", large: "1000x600#" }
   validates_attachment :picture, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 
   validates :title, presence: true
