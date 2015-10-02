99d4bbc1305ba98455a9c21fa6ea7804002c26d0
tslater3 <trevor.s.slater@gmail.com>
2015-10-01 20:43:21 -0600

    added after update redirect/after sign out redirect

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,6 +6,10 @@ class RegistrationsController < Devise::RegistrationsController
     def after_sign_in_path_for(resource)
       users_path
     end
+
+    def after_update_path_for(resource)
+      user_path(current_user.id)
+    end
   private
   def sign_up_params
     params.require(:user).permit(:user_name, :description, :days_left, :email, :password, :password_confirmation, :avatar)
