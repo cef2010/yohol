1363aa6aa2cf50b495565499aff1e1ed41a1bca4
tslater3 <trevor.s.slater@gmail.com>
2015-10-01 20:38:19 -0600

    added redirect to users after sign in/sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!, except: [:about, :faq, :contact, :submit_contact_info]
+  def after_sign_in_path_for(resource)
+    users_path
+  end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,11 @@
 class RegistrationsController < Devise::RegistrationsController
+    def after_sign_up_path_for(resource)
+      users_path
+    end
 
+    def after_sign_in_path_for(resource)
+      users_path
+    end
   private
   def sign_up_params
     params.require(:user).permit(:user_name, :description, :days_left, :email, :password, :password_confirmation, :avatar)
